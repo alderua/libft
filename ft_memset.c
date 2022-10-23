5ea1652f299f9a916f751045f930c4c56bd54984
Ibai Aldecoa De La Rua <ialdecoa@student.42madrid.com>
2022-10-23 12:18:24 +0200

    memset sin terminar

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialdecoa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 11:58:15 by ialdecoa          #+#    #+#             */
+/*   Updated: 2022/10/23 12:13:52 by ialdecoa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)b;
+	while (len-- > 0)
+		*(str++) = (unsigned char)c;
+	return (b);
+}
